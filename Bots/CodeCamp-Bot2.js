6b073d88236ff637d1058e09f8e5ef01faef40fb
Stephen Hall <hall.stephenk@gmail.com>
2018-07-18 23:16:02 -0400

    Bot2 - the SUPERBOT!

--- Bots/CodeCamp-Bot2.js
+++ Bots/CodeCamp-Bot2.js
@@ -3,89 +3,8 @@
 var command = {
     action: null,
     direction: null,
-    message: "trying",
+    message: '',
 };
-
-var imFacing = "south";
-var lookedRight = false;
-var lookedLeft = false;
-
-function openExit(direction, sight) {
-    var exits = getExits(sight);
-
-    if (direction == "north" && exits.north) {
-        return true;
-    }
-    if (direction == "south" && exits.south) {
-        return true;
-    }
-    if (direction == "east" && exits.east) {
-        return true;
-    }
-    if (direction == "west" && exits.west) {
-        return true;
-    }
-
-    return false;
-}
-
-function getExits(sight) {
-    var exits = {
-        north: false,
-        south: false,
-        east: false,
-        west: false,
-    };
-    
-    if (sight.includes("exit")) {
-        if (sight.includes("north")) {
-            exits.north=true;
-        }
-        if (sight.includes("south")) {
-            exits.south=true;
-        }
-        if (sight.includes("east")) {
-            exits.east=true;
-        }
-        if (sight.includes("west")) {
-            exits.west=true;
-        }
-    }
-
-    return exits;
-}
-
-function getRight(direction) {
-    if ("north" == direction) {
-        return "east";
-    }
-    if ("east" == direction) {
-        return "south";
-    }
-    if ("south" == direction) {
-        return "west";
-    }
-    if ("west" == direction) {
-        return "north";
-    }
-    return "north"; // don't know!   
-}
-
-function getLeft(direction) {
-    if ("north" == direction) {
-        return "west";
-    }
-    if ("east" == direction) {
-        return "north";
-    }
-    if ("south" == direction) {
-        return "east";
-    }
-    if ("west" == direction) {
-        return "south";
-    }
-    return "south"; // don't know!   
-}
 
 module.exports = {
     /**
@@ -97,57 +16,67 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        if (null == gameState) {
-            command.action='look';
-            command.direction=imFacing;
-        } else {
-                // let's see what we saw
-                if (!gameState.engram.sight.toLowerCase().includes('lava') &&
-                    !gameState.engram.sight.toLowerCase().includes('wall')) {
-                    // no lava, no wall, that's good!
+        command.action = null;
+        command.direction = null;
 
-                    if ("look" == command.action) {
-                        // if our last action was looking in a direction, go ahead and move that way
-                        imFacing = command.direction;
-                        command.action = "move";
-                        lookedLeft = false;
-                        lookedRight = false;
-                    } else {
-                        // we're in motion, so use the sight sense to keep moving quickly
-                        if (openExit(getRight(imFacing), gameState.engram.sight)) {
-                            imFacing = getRight(imFacing);
-                            command.action = "move";
-                            command.direction = imFacing;
-                        } else if (openExit(imFacing, gameState.engram.sight)) {
-                            command.action = "move";
-                            command.direction = imFacing;
-                        } else if (openExit(getLeft(imFacing), gameState.engram.sight)) {
-                            imFacing = getLeft(imFacing);
-                            command.action = "move";
-                            command.direction = imFacing;
-                        } else {
-                            imFacing = getRight(getRight(imFacing));
-                            command.action = "move";
-                            command.direction = imFacing;
-                        }
-                    }
-                } else {
-                    if (!lookedRight) {
-                        lookedRight = true;
-                        command.action = "look";
-                        command.direction = getRight(imFacing);
-                    } else if (!lookedLeft) {
-                        lookedLeft = true;
-                        command.action = "look";
-                        command.direction = getLeft(imFacing);
-                    } else {
-                        command.action = "move"; // turn around!
-                        command.direction = getRight(getRight(imFacing));
-                        imFacing = command.direction;
-                    }
-                }
+        if (null != gameState) {
+            var northExit = false;
+            var southExit = false;
+            var eastExit = false;
+            var westExit = false;
+
+            if (gameState.engram.sight.includes('north')) northExit = true;
+            if (gameState.engram.sight.includes('south')) southExit = true;
+            if (gameState.engram.sight.includes('east')) eastExit = true;
+            if (gameState.engram.sight.includes('west')) westExit = true;
+
+            if (gameState.engram.smell.includes('molten rock') && gameState.engram.smell.includes('north')) northExit = false;
+            if (gameState.engram.smell.includes('molten rock') && gameState.engram.smell.includes('south')) southExit = false;
+            if (gameState.engram.smell.includes('molten rock') && gameState.engram.smell.includes('east')) eastExit = false;
+            if (gameState.engram.smell.includes('molten rock') && gameState.engram.smell.includes('west')) westExit = false;
+
+            if (gameState.action == 'MOVE') {
+                // avoid going backward!
+                if (gameState.direction == 'NORTH') southExit = false;
+                if (gameState.direction == 'SOUTH') northExit = false;
+                if (gameState.direction == 'EAST') westExit = false;
+                if (gameState.direction == 'WEST') eastExit = false;
             }
-        
+
+            // now determine where we can go!
+            if (northExit) {
+                command.action = 'move';
+                command.direction = 'north';
+            }
+            if (southExit) {
+                command.action = 'move';
+                command.direction = 'south';
+            }
+            if (eastExit) {
+                command.action = 'move';
+                command.direction = 'east';
+            }
+            if (westExit) {
+                command.action = 'move';
+                command.direction = 'west';
+            }
+
+            console.log('Move #' + gameState.score.moveCount);
+            console.log('  sight: ' + gameState.engram.sight);
+            console.log('  smell: ' + gameState.engram.smell);
+            console.log('  sound: ' + gameState.engram.sound);
+            console.log('  taste: ' + gameState.engram.taste);
+            console.log('  touch: ' + gameState.engram.touch);
+        }
+
+        if (command.action == null || command.direction == null) {
+            command.action = 'look';
+            command.direction = 'none';
+        }
+
+        console.log('  action: ' + command.action);
+        console.log('  direction: ' + command.direction);
+
         return command;
 
         // *********************************************************************
